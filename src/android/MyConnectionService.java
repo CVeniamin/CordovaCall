e9e9c4dbe983257b59aa0886f7d5e14683e79cd0
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-04-23 23:34:52 +0100

    Update MyConnectionService.java

--- src/android/MyConnectionService.java
+++ src/android/MyConnectionService.java
@@ -93,7 +93,9 @@ public class MyConnectionService extends ConnectionService {
             }
         };
         //  connection.setAddress(Uri.parse(request.getExtras().getString("from")), TelecomManager.PRESENTATION_ALLOWED);
-        connection.setCallerDisplayName(request.getExtras().getString("from"), TelecomManager.PRESENTATION_ALLOWED);
+        //  connection.setCallerDisplayName(request.getExtras().getString("from"), TelecomManager.PRESENTATION_ALLOWED);
+        connection.setAddress(Uri.parse(request.getExtras().getString("from")), TelecomManager.PRESENTATION_UNKNOWN);
+        connection.setCallerDisplayName(request.getExtras().getString("from"), TelecomManager.PRESENTATION_UNKNOWN);
         Icon icon = CordovaCall.getIcon();
         if(icon != null) {
             StatusHints statusHints = new StatusHints((CharSequence)"", icon, new Bundle());
@@ -165,7 +167,11 @@ public class MyConnectionService extends ConnectionService {
             }
         };
         // connection.setAddress(Uri.parse(request.getExtras().getString("to")), TelecomManager.PRESENTATION_ALLOWED);
-        connection.setCallerDisplayName(request.getExtras().getString("to"), TelecomManager.PRESENTATION_ALLOWED);
+        //  connection.setCallerDisplayName(request.getExtras().getString("to"), TelecomManager.PRESENTATION_ALLOWED);
+        
+        connection.setAddress(Uri.parse(request.getExtras().getString("to")), TelecomManager.PRESENTATION_UNKNOWN);
+        connection.setCallerDisplayName(request.getExtras().getString("to"), TelecomManager.PRESENTATION_UNKNOWN);
+        
         Icon icon = CordovaCall.getIcon();
         if(icon != null) {
             StatusHints statusHints = new StatusHints((CharSequence)"", icon, new Bundle());
